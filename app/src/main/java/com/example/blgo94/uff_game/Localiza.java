0341f3608109eb8e1586aa56c773b8346e71ce0e
BrLocha <brunolocha2@gmail.com>
2018-11-25 03:58:42 -0200

    "Update 39, Lista de usuarios proximos foi melhorada"

--- app/src/main/java/com/example/blgo94/uff_game/Localiza.java
+++ app/src/main/java/com/example/blgo94/uff_game/Localiza.java
@@ -13,6 +13,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -20,12 +21,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -35,9 +41,8 @@ public class Localiza extends AppCompatActivity implements LocationListener{
     final String TAG = "GPS";
     private final static int ALL_PERMISSIONS_RESULT = 101;
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10
-    private static final long MIN_TIME_BW_UPDATES = 100; // 1000 * 60 * 1
+    private static final long MIN_TIME_BW_UPDATES = 1000; // 1000 * 60 * 1
 
-    TextView tvLatitude, tvLongitude, tvTime;
     LocationManager locationManager;
     Location loc;
     ArrayList<String> permissions = new ArrayList<>();
@@ -47,11 +52,28 @@ public class Localiza extends AppCompatActivity implements LocationListener{
     boolean isNetwork = false;
     boolean canGetLocation = true;
 
-    DatabaseReference data;
-
     private String user_name;
 
+    DatabaseReference data;
+    DatabaseReference data_users;
+    DatabaseReference data_amigos;
+
     private int contador = 0;
+
+    private ListView lista;
+
+    private Localizacao loc_usuario = new Localizacao();
+    private Localizacao loc_proximo = new Localizacao();
+
+    private ArrayList<String> ids_amigos = new ArrayList<String>();
+
+    private ArrayList<String> ids_proximos = new ArrayList<String>();
+
+    private ArrayList<Usuario> users = new ArrayList<Usuario>();
+
+    TextView texto_lista;
+
+    private double raio = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +89,9 @@ public class Localiza extends AppCompatActivity implements LocationListener{
             finish();
         }
 
-        tvLatitude = (TextView) findViewById(R.id.tvLatitude);
-        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
-        tvTime = (TextView) findViewById(R.id.tvTime);
-
         user_name = (String) getIntent().getStringExtra("ID_USUARIO");
+
+        texto_lista = (TextView)  findViewById(R.id.texto_lista_usuarios);
 
         locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
         isGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
@@ -271,9 +291,6 @@ public class Localiza extends AppCompatActivity implements LocationListener{
 
     private void updateUI(Location loc) {
         Log.d(TAG, "updateUI");
-        tvLatitude.setText(Double.toString(loc.getLatitude()));
-        tvLongitude.setText(Double.toString(loc.getLongitude()));
-        tvTime.setText(DateFormat.getTimeInstance().format(loc.getTime()));
 
         //ATUALIZA DATABASE
 
@@ -287,16 +304,176 @@ public class Localiza extends AppCompatActivity implements LocationListener{
         data.child(user_name).setValue(atual);
 
         if(contador == 0){
+
+            data_amigos = FirebaseDatabase.getInstance().getReference("amigos");
+
+            data_amigos.child(user_name).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    preenche_array_amigos(dataSnapshot);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            data = FirebaseDatabase.getInstance().getReference("loc");
+
+            data.child(user_name).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    loc_usuario = dataSnapshot.getValue(Localizacao.class);
+                    checa_usuarios_proximos();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
             //Inicia activity de lista
+            /*
             Intent intent = new Intent(Localiza.this, Lista_usuarios_proximos.class);
             intent.putExtra("ID_USUARIO", user_name);
             startActivity(intent);
+            */
             contador ++;
         }
 
-        //CASO ACHE UMA MANEIRA DE ATUALIZAR INSTANTANEAMENTE QUANDO ENTRAR NESSA TELA, FINALIZAR
-        //finish();
     }
+
+    private void preenche_array_amigos(DataSnapshot dataSnapshot){
+        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+            ids_amigos.add(snapshot.getKey());
+        }
+    }
+
+    private void checa_usuarios_proximos(){
+        data.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                preenche_array(dataSnapshot);
+                busca_users();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void busca_users(){
+
+        data_users = FirebaseDatabase.getInstance().getReference("users");
+
+        data_users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                set_users(dataSnapshot);
+                set_adaptador();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void set_adaptador(){
+        lista = (ListView) findViewById(R.id.lista_usuarios_proximos);
+
+        if(users.size() > 0) {
+
+            lista.setVisibility(View.VISIBLE);
+            texto_lista.setVisibility(View.GONE);
+
+            List_adapter_usuarios adaptador = new List_adapter_usuarios(this, R.layout.estilo_amigos_lista, users);
+
+            lista.setAdapter(adaptador);
+
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Usuario user_ref = (Usuario) parent.getItemAtPosition(position);
+
+                    Intent intent = new Intent(Localiza.this, Perfil.class);
+                    intent.putExtra("objeto", user_ref);
+                    if (valida(user_ref.getID())) {
+                        intent.putExtra("caso", "0");
+                    } else {
+                        intent.putExtra("caso", "1");
+                    }
+                    intent.putExtra("ID_USUARIO", user_name);
+                    startActivity(intent);
+                }
+            });
+        }
+        else{
+            texto_lista.setText(R.string.nenhum_usuario);
+        }
+    }
+
+    private boolean valida(String alvo_id){
+        for(int i = 0; i < ids_amigos.size(); i++){
+            if (ids_amigos.get(i).equals(alvo_id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void set_users(DataSnapshot dataSnapshot){
+        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            for(int i = 0; i < ids_proximos.size(); i++){
+                if(snapshot.getKey().equals(ids_proximos.get(i))){
+                    users.add(snapshot.getValue(Usuario.class));
+                }
+            }
+        }
+    }
+
+    private void preenche_array(DataSnapshot dataSnapshot){
+        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            loc_proximo = snapshot.getValue(Localizacao.class);
+            if(!loc_proximo.getId().equals(loc_usuario.getId())) {
+                if (distance(Double.parseDouble(loc_usuario.getLocalizacoes().get(0)), Double.parseDouble(loc_usuario.getLocalizacoes().get(1)),
+                        Double.parseDouble(loc_proximo.getLocalizacoes().get(0)), Double.parseDouble(loc_proximo.getLocalizacoes().get(1)))
+                        <= raio) {
+                    ids_proximos.add(loc_proximo.getId());
+                }
+            }
+        }
+    }
+
+
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+
 
     @Override
     protected void onDestroy() {
